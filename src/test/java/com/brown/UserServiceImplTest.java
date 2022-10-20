64d86198a0d595d5749b33cba6e8ffe44b605667
Amatullah Brown <amatullah.brown@gmail.com>
2022-10-20 10:46:51 -0400

    updated tests

--- src/test/java/com/brown/UserServiceImplTest.java
+++ src/test/java/com/brown/UserServiceImplTest.java
@@ -3,8 +3,10 @@ package com.brown;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.brown.model.User;
@@ -14,14 +16,25 @@ import com.brown.service.UserServiceImpl;
 @SpringBootTest
 public class UserServiceImplTest {
     
-//    @BeforeAll
-//    UserService userService = new UserServiceImpl();
-    User user = new User();
-    
-    
     @ParameterizedTest
-    @EmptySource
-    void getAllUsersTest () {
+    @ValueSource(strings = {"John"})
+    void checkUsername (String name) {
+        UserService userService = new UserServiceImpl();
+        // given
+        User user = new User();
+        user.setFirstname("John");
+        user.setLastname("Smith");
+        user.setEmail("john@gmail.com");
+        user.setPassword("password");
+        userService.addUser(user);
+        // expect
+        User john = userService.getUserById(user.getId());
+        
+        assertTrue(john.getFirstname() == name);
+    }
+    
+    @Test
+    void getAllUsersTest (User existingUser) {
         UserService userService = new UserServiceImpl();
         assertTrue(userService.getAllUsers().size() == 0);
     }
